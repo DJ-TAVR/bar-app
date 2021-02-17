b4326dae043df320b0435b94edc0e4c78be7e196
Tuan Huynh <thuynh71@gatech.edu>
2021-02-17 14:39:35 -0500

    fix circular import

--- back-end/stickers/models.py
+++ back-end/stickers/models.py
@@ -5,11 +5,11 @@ class Bar(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=5)
-    manager = models.OneToOneField(User, on_delete=models.SET_NULL)
+    manager = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
 
 class Sticker(models.Model):
     drink_name = models.CharField(max_length=50, blank=True, null=True)
     drink_type = models.CharField(max_length=50, blank=True, null=True)
     drink_size = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(decimal_places=2, blank=True, null=True)
-    bar = models.ForeignKey(Bar, related_name='stickers', on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    bar = models.ForeignKey(Bar, related_name='stickers', on_delete=models.SET_NULL, null=True)
