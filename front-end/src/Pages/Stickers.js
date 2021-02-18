cec4d114272c70da4db1fc82811cbccbd5f65c00
GratefulDeadpool <dashcc1999@gmail.com>
2021-02-18 17:10:53 -0500

    Table design overhaul; sticker table dynamically resizes based on object input

--- front-end/src/Pages/Stickers.js
+++ front-end/src/Pages/Stickers.js
@@ -3,24 +3,22 @@ import React, {useState, useEffect} from "react";
 import Table from 'react-bootstrap/Table'
 import { Button } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom'
+import { useTable } from 'react-table'
 
-class Stickers extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
-
-    // Adapted from https://stackoverflow.com/questions/33267797/
-    editData = (e) => {
-        const el = e.target;
+function editData(e) {
+    const el = e.target;
+    const index = Array.prototype.indexOf.call(el.parentNode.children, el);
+    const th = document.querySelector('#stickers th:nth-child(' + (index+1) + ')');
+    if (!th.textContent.includes("Sticker ID")) {
         const input = document.createElement("input");
         input.setAttribute("value", el.textContent);
+        input.className = "StickerInput";
         el.replaceWith(input);
         const save = function() {
             const previous = document.createElement(el.tagName.toLowerCase());
-            previous.onClick = this.editData;
+            previous.onClick = {editData};
             previous.textContent = input.value.trim();
-            previous.className = previous.textContent ? "stickerElement": "nullStickerElement";
+            previous.className = "StickerElement";
             input.replaceWith(previous);
         };
         input.addEventListener('blur', save, {
@@ -28,51 +26,86 @@ class Stickers extends React.Component {
         });
         input.focus();
     }
+}
 
-    render() {
-        return(
-            <div className="Stickers">
+export default function Stickers(props) {
+    const columns = React.useMemo(() => [{
+        Header: "Sticker ID",
+        accessor: "id" 
+    }, {
+        Header: "Brand",
+        accessor: "brand",
+    }, {
+        Header: "Drink Type",
+        accessor: "type",
+    }, {
+        Header: "MLPP (L)",
+        accessor: "mlpp",
+    }, {
+        Header: "Price Per Liter (L)",
+        accessor: "price",
+    }], []);
+
+    let data = React.useMemo(() => [{
+        id: "NCC-1701",
+        brand: "Captain Morgan",
+        type: "rum",
+        mlpp: "3",
+        price: "5"
+    }, {
+        id: "NCC-1337",
+        brand: "",
+        type: "",
+        mlpp: "",
+        price: ""
+    }], []);
+
+    let {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data });
+
+    return(
+        <div className="Stickers">
             <Button className = "HelpButton"> ? </Button>
             <Link to = "/admin">
                 <Button className = "BackAdminButton">Back to Dashboard</Button>
             </Link>
             <h1 class = "Table_Text">Manage Stickers</h1>
-            <Table className = "Table-header">
-            <colgroup>
-              <col class = "green"/>
-            </colgroup>
-            <thead>
-                <tr >
-                <th>Sticker ID</th>
-                <th>Brand</th>
-                <th>Drink Type</th>
-                <th>MLPP (L)</th>
-                <th>Price per Liter($)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>NCC-1701</td>
-                <td class="stickerElement" onClick={this.editData}>Captain Morgan</td>
-                <td class="stickerElement" onClick={this.editData}>Rum</td>
-                <td class="stickerElement" onClick={this.editData}>3</td>
-                <td class="stickerElement" onClick={this.editData}>5</td>
-                </tr>
-    
-                <tr>
-                <td>NCC-1337</td>
-                <td class="nullStickerElement" onClick={this.editData}></td>
-                <td class="nullStickerElement" onClick={this.editData}></td>
-                <td class="nullStickerElement" onClick={this.editData}></td>
-                <td class="nullStickerElement" onClick={this.editData}></td>
-                </tr>
-    
-            </tbody>
+            <Table {...getTableProps()} className = "Table-header" id="stickers">
+                <colgroup>
+                    <col class = "green"/>
+                </colgroup>
+                <thead> {
+                    headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}> {
+                            headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}> {column.render('Header')} </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}> {
+                    rows.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}> {
+                                row.cells.map(cell => {
+                                    return (<td {...cell.getCellProps()} 
+                                                className="StickerElement"
+                                                onClick={editData}> 
+                                                    {cell.render('Cell')} 
+                                            </td>
+                                        );
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
             </Table>
-            
-            </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default withRouter(Stickers);
