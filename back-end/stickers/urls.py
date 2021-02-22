37cd55c7f02e907ff877e53adfe52541aef23c8d
catinabox9 <77685967+catinabox9@users.noreply.github.com>
2021-02-22 14:50:14 -0500

    add urls

--- back-end/stickers/urls.py
+++ back-end/stickers/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('get/', views.get_stickers_view, name='api-get-stickers'),
+    path('create/', views.create_sticker_view, name='api-create-stickers'),
 ]
