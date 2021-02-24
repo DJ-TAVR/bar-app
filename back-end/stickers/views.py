e8e4a583f9e7d47a37f261d9c63c50850915b1d1
Iyer, Akshay R <aiyer89@gatech.edu>
2021-02-24 00:20:44 -0600

    Basic working Put request for updating stickers

--- back-end/stickers/views.py
+++ back-end/stickers/views.py
@@ -32,9 +32,37 @@ def create_sticker_view(request):
             else:
                 return JsonResponse({'detail': 'Failed to create new sticker.'}, status = 400)
 
-@api_view(['PUT'])
+@api_view(['GET', 'PUT'])
 def update_sticker_view(request):
-    pass
+    user = request.user
+    #if check_bar_manager_access(user):
+    if request.method == 'GET':
+        group = Group.objects.get(name='Bar Manager')
+        correct_bar = Bar.objects.get(manager = user)
+        stickers = Sticker.objects.filter(bar=correct_bar)
+        serialized_stickers = StickerSerializer(stickers, many = True)
+        return Response(serialized_stickers.data, status=200)
+        #return get_all_stickers(user)
+    elif request.method == 'PUT':
+        instance = Sticker.objects.values('sticker_id')
+        stickerInTable = 0
+        for i in range(len(instance)):
+            if instance[i]['sticker_id'] == request.data['sticker_id']:
+                stickerInTable = 1
+
+        if stickerInTable == 1:
+            object_to_update = Sticker.objects.get(pk = request.data['sticker_id'])
+            object_to_update.drink_name = request.data['drink_name']
+            object_to_update.drink_type = request.data['drink_type']
+            object_to_update.drink_size = request.data['drink_size']
+            object_to_update.price = request.data['price']
+            object_to_update.save()
+
+            return JsonResponse({'detail': 'Successfully Updated Sticker!'}, status = 200)
+        else:
+            return JsonResponse({'detail': 'Failed to Update Sticker'}, status = 400)
+        
+    
 
 @api_view(['GET', 'POST'])
 def delete_sticker_view(request):
@@ -46,8 +74,9 @@ def delete_sticker_view(request):
         stickers = Sticker.objects.filter(bar=correct_bar)
         serialized_stickers = StickerSerializer(stickers, many = True)
         return Response(serialized_stickers.data, status=200)
+        #return get_all_stickers(user)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         try:
             currSticker = request.data["sticker_id"]
             object_to_delete = Sticker.objects.get(pk = currSticker)
