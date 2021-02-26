0160e3104cfc4e6c718ce9f013d8cabd925db148
GratefulDeadpool <dashcc1999@gmail.com>
2021-02-25 21:28:45 -0500

    Added demonstrative 'addSticker' and 'removeSticker' buttons

--- front-end/src/Pages/Stickers.js
+++ front-end/src/Pages/Stickers.js
@@ -7,75 +7,6 @@ import { useTable, useSortBy } from 'react-table'
 
 var selector = 0;
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-function editData(e) {
-    const el = e.target;
-    const index = Array.prototype.indexOf.call(el.parentNode.children, el);
-    const th = document.querySelector('#stickers th:nth-child(' + (index+1) + ')');
-    selector += 1;
-    if (!th.textContent.includes("Sticker ID")) {
-        el.innerHTML = "<input class='StickerInput' id='n" + selector + "' value='" + el.textContent + "'> </input>";
-        document.getElementById("n"+selector).focus();
-    }
-    document.getElementById("submitChanges").style.visibility = "visible";
-}
-
-function submit() {
-    let csrf = getCookie('csrftoken');
-    const table = document.getElementById("stickers");
-    selector = 0;
-    for (let i = 1, row; row = table.rows[i]; i++) {
-        let changes = []
-        let changeHappens = false;
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            if (col.firstChild.className == "StickerInput") {
-                changes.push(col.firstChild.value)
-                col.innerHTML = col.firstChild.value
-                changeHappens = true;
-            } else {
-                changes.push(col.innerHTML)
-            }
-        }
-        if (changeHappens) {
-            fetch("http://localhost:8000/sticker/update/", {
-                method: "PUT",
-                headers: {
-                    "X-CSRFToken": csrf ,
-                    "Content-Type": "application/json"
-                },
-                credentials: "include",
-                sticker_id: changes[0],
-                body: JSON.stringify({
-                    sticker_id: changes[0],
-                    drink_name: changes[1],
-                    drink_type: changes[2],
-                    drink_size: changes[3],
-                    price: changes[4]
-                })
-            })
-            .then((res) => {
-                console.log(res)
-            })
-        }
-    }
-    document.getElementById("submitChanges").style.visibility = "hidden";
-}
-
 export default function Stickers(props) {
     const [data, setData] = React.useState([]);
 
@@ -96,7 +27,76 @@ export default function Stickers(props) {
         accessor: "price",
     }], []);
 
-    useEffect(() => {
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    
+    function editData(e) {
+        const el = e.target;
+        const index = Array.prototype.indexOf.call(el.parentNode.children, el);
+        const th = document.querySelector('#stickers th:nth-child(' + (index+1) + ')');
+        selector += 1;
+        if (!th.textContent.includes("Sticker ID")) {
+            el.innerHTML = "<input class='StickerInput' id='n" + selector + "' value='" + el.textContent + "'> </input>";
+            document.getElementById("n"+selector).focus();
+        }
+        document.getElementById("submitChanges").style.visibility = "visible";
+    }
+    
+    function submit() {
+        let csrf = getCookie('csrftoken');
+        const table = document.getElementById("stickers");
+        selector = 0;
+        for (let i = 1, row; row = table.rows[i]; i++) {
+            let changes = []
+            let changeHappens = false;
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                if (col.firstChild.className == "StickerInput") {
+                    changes.push(col.firstChild.value)
+                    col.innerHTML = col.firstChild.value
+                    changeHappens = true;
+                } else {
+                    changes.push(col.innerHTML)
+                }
+            }
+            if (changeHappens) {
+                fetch("http://localhost:8000/sticker/update/", {
+                    method: "PUT",
+                    headers: {
+                        "X-CSRFToken": csrf ,
+                        "Content-Type": "application/json"
+                    },
+                    credentials: "include",
+                    sticker_id: changes[0],
+                    body: JSON.stringify({
+                        sticker_id: changes[0],
+                        drink_name: changes[1],
+                        drink_type: changes[2],
+                        drink_size: changes[3],
+                        price: changes[4]
+                    })
+                })
+                .then((res) => {
+                    console.log(res)
+                })
+            }
+        }
+        document.getElementById("submitChanges").style.visibility = "hidden";
+    }
+
+    function updateTable() {
         let csrf = getCookie('csrftoken');
         fetch("http://localhost:8000/sticker/get/", {
             headers: {
@@ -105,6 +105,56 @@ export default function Stickers(props) {
             credentials: "include"
         }).then(r =>  r.json().then(data => ({status: r.status, body: data})))
         .then(obj => setData(obj.body));
+    }
+
+    function addSticker() {
+        let csrf = getCookie('csrftoken');
+        fetch("http://localhost:8000/sticker/create/", {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": csrf ,
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                sticker_id: (Math.floor(Math.random() * 100) + 1).toString(),
+                drink_name: "test",
+                drink_type: "lemon",
+                drink_size: "12",
+                price: "3.50"
+            })
+        })
+        .then((res) => {
+            console.log(res)
+        }).then(() =>{
+            updateTable();
+        })
+    }
+
+    function removeSticker() {
+        let csrf = getCookie('csrftoken');
+        let lastRow = document.getElementById("stickers").rows;
+        let lastID = lastRow[lastRow.length - 1].children[0].innerText;
+        fetch("http://localhost:8000/sticker/delete/", {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": csrf ,
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                sticker_id: lastID,
+            })
+        })
+        .then((res) => {
+            console.log(res)
+        }).then(() =>{
+            updateTable();
+        })
+    }
+    
+    useEffect(() => {
+        updateTable();
     }, []);
 
     let {
@@ -157,6 +207,9 @@ export default function Stickers(props) {
                 </tbody>
             </Table>
             <Button className = "button managerButton" id="submitChanges" onClick={submit}>Submit</Button>
+            {/* Demonstration Commands */}
+            <Button onClick={addSticker}>Add Sticker to Bar</Button>
+            <Button onClick={removeSticker}>Remove Sticker from Bar</Button>
         </div>
     )
 }
