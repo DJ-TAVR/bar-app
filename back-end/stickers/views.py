a4c5094d08399b35d6564035c08ed52a5cd8468a
catinabox9 <77685967+catinabox9@users.noreply.github.com>
2021-03-15 23:56:45 -0400

    auto format

--- back-end/stickers/views.py
+++ back-end/stickers/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from django.contrib.auth.models import Group
-from .models import Sticker, Bar
+from .models import Sticker, Bar, Shift
 from .serializers import StickerSerializer
 from rest_framework.response import Response
 from django.http import JsonResponse
 from django.shortcuts import redirect
 import json
 from .forms import StickerForm
+from heapq import nlargest
 from rest_framework.permissions import IsAuthenticated
 
-# Stickers 
+
+# Stickers
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_stickers_view(request):
     return get_all_stickers(request.user)
-    
+
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -30,9 +32,9 @@ def create_sticker_view(request):
             if serializer.is_valid():
                 instance = serializer.save(bar=correct_bar)
                 instance.save()
-                return JsonResponse({'detail': 'Successfully created stickers'}, status = 200)
+                return JsonResponse({'detail': 'Successfully created stickers'}, status=200)
             else:
-                return JsonResponse({'detail': 'Failed to create new sticker.'}, status = 400)
+                return JsonResponse({'detail': 'Failed to create new sticker.'}, status=400)
 
 
 @api_view(['GET', 'PUT'])
@@ -49,17 +51,18 @@ def update_sticker_view(request):
                 if instance[i]['sticker_id'] == request.data['sticker_id']:
                     stickerInTable = 1
             if stickerInTable == 1:
-                object_to_update = Sticker.objects.get(pk = request.data['sticker_id'])
+                object_to_update = Sticker.objects.get(pk=request.data['sticker_id'])
                 object_to_update.drink_name = request.data['drink_name']
                 object_to_update.drink_type = request.data['drink_type']
                 object_to_update.drink_size = request.data['drink_size']
                 object_to_update.price = request.data['price']
                 object_to_update.save()
 
-                return JsonResponse({'detail': 'Successfully Updated Sticker!'}, status = 200)
+                return JsonResponse({'detail': 'Successfully Updated Sticker!'}, status=200)
             else:
-                return JsonResponse({'detail': 'Failed to Update Sticker'}, status = 400)
-        
+                return JsonResponse({'detail': 'Failed to Update Sticker'}, status=400)
+
+
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def delete_sticker_view(request):
@@ -70,23 +73,22 @@ def delete_sticker_view(request):
         elif request.method == 'POST':
             try:
                 currSticker = request.data["sticker_id"]
-                object_to_delete = Sticker.objects.get(pk = currSticker)
+                object_to_delete = Sticker.objects.get(pk=currSticker)
                 object_to_delete.delete()
-                return JsonResponse({'detail': 'Successfully Deleted Sticker!'}, status = 200)
+                return JsonResponse({'detail': 'Successfully Deleted Sticker!'}, status=200)
             except:
-                return JsonResponse({'detail': 'Failed to Delete Sticker'}, status = 400)
+                return JsonResponse({'detail': 'Failed to Delete Sticker'}, status=400)
 
 
 def get_all_stickers(user):
     group = Group.objects.get(name='Bar Manager')
-    correct_bar = Bar.objects.get(manager = user)
+    correct_bar = Bar.objects.get(manager=user)
     if group in user.groups.all():
         stickers = Sticker.objects.filter(bar=correct_bar)
-        serialized_stickers = StickerSerializer(stickers, many = True)
+        serialized_stickers = StickerSerializer(stickers, many=True)
         return Response(serialized_stickers.data, status=200)
     else:
-        return JsonResponse({'detail': 'Insufficient privilege.'}, status = 400)
-
+        return JsonResponse({'detail': 'Insufficient privilege.'}, status=400)
 
 
 def check_bar_manager_access(user):
@@ -94,15 +96,18 @@ def check_bar_manager_access(user):
     if group in user.groups.all():
         return True
     else:
-        return JsonResponse({'detail': 'Insufficient privilege.'}, status = 400)
+        return JsonResponse({'detail': 'Insufficient privilege.'}, status=400)
+
 
 # stat
 ## helper functions
 def calculate_avg_MLPP(list_of_shifts):
     return 0
 
+
 def calculate_cumulative_MLPP(list_of_shifts):
     return 0
+
 
 def find_top3_shifts_MLPP(list_of_shifts):
     limit = min(3, len(list_of_shifts))
