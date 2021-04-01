1a7cc27545db22ce2321ea17039e305f6e386462
vibhavasan <51873332+vibhavasan@users.noreply.github.com>
2021-03-31 22:13:03 -0400

    Add files via upload

--- front-end/src/Components/CustomSidebar.js
+++ front-end/src/Components/CustomSidebar.js
@@ -1,8 +1,14 @@
-import React from 'react'
+
 import '../App.css'
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarFooter, SidebarContent } from 
 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
+import { Button } from 'reactstrap';
+import ReactDOM from 'react-dom';
+import React, {useState, useEffect} from "react";
+import {TextField} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 
 import { Link } from 'react-router-dom'
@@ -15,22 +21,34 @@ export default function CustomSidebar(props){
     <ProSidebar popperArrow = "true" className = "sidebarSize" width = "175px">
         <SidebarContent>
         <Menu iconShape = "square">
-            <MenuItem class = "normal"></MenuItem>
-          <MenuItem>Help</MenuItem>
+          <MenuItem class = "normal"></MenuItem>
+        
           <SubMenu title = "Statistics">
             <MenuItem><Link to = "/bartender">Bartender</Link></MenuItem>
             <MenuItem><Link to = "/drinks">Drinks</Link></MenuItem>
           </SubMenu>
           <MenuItem><Link to = "/stickers">Manage Stickers</Link></MenuItem>
+          <MenuItem><Link to = "/addBartender">Add Bartender</Link></MenuItem>
           <MenuItem><Link to = "/registration">Registration</Link></MenuItem>
+          <MenuItem><Link to = "/help">Help</Link></MenuItem>
         </Menu>
         </SidebarContent>
         <SidebarFooter>
             <Menu>
-                <MenuItem><Link to = "/">Log Out</Link></MenuItem>
+                <MenuItem onClick = {logoutButton}><Link to = "/">Logout</Link></MenuItem>
             </Menu>
         </SidebarFooter>
       </ProSidebar>
     </div>
     )
+
+    function logoutButton(){
+        fetch("http://localhost:8000/logout/")
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err)=> {
+            console.error(err)
+        });
+    }
 }
