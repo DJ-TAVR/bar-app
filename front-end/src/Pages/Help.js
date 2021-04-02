abb6c5505188794bf8d52e500b45994a346b9d39
GratefulDeadpool <dashcc1999@gmail.com>
2021-04-02 13:50:32 -0400

    Added styling fixes (TODO: Finish AddBartender)

--- front-end/src/Pages/Help.js
+++ front-end/src/Pages/Help.js
@@ -11,11 +11,11 @@ export default function Help(props){
         <CustomSidebar isAuth = {props.isAuth} setIsAuth = {props.setIsAuth} csrfToken = {props.csrfToken} setCSRFToken = {props.setCSRFToken}/>
         <div class = "stay center">
         <h1 className = "Table_Text"> Help </h1>
-        <h1 className = "Table_Text"> Use the sidebar to navigate through this application. </h1>
-        <h1 className = "Table_Text"> Click on Statistics -> Bartender to view statistics on the bartenders </h1>
-        <h1 className = "Table_Text"> Click on Statistics -> Drinks to view properties of the various drinks </h1>
-        <h1 className = "Table_Text"> Click on Manage stickers to assign stickers to various bar codes </h1>
-        <h1 className = "Table_Text"> Click on Registration to register as a bartender on this app</h1>
+        <h6 className = "Table_Text"> Use the sidebar to navigate through this application. </h6>
+        <h6 className = "Table_Text"> Click on Statistics -> Bartender to view statistics on the bartenders </h6>
+        <h6 className = "Table_Text"> Click on Statistics -> Drinks to view properties of the various drinks </h6>
+        <h6 className = "Table_Text"> Click on Manage stickers to assign stickers to various bar codes </h6>
+        <h6 className = "Table_Text"> Click on Registration to register as a bartender on this app</h6>
         </div>
         </div>
     )
